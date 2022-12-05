f68db8452b2c2eacb6f487cbca3a3fca4c35e57a
gaugeL <63837583+gaugeL@users.noreply.github.com>
2022-12-05 00:22:10 -0600

    2022 day 2

---
+++ 2022/2/b.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+let rpsList = fs.readFileSync("input.txt", "utf8").trim().split("\n");
+let ans = 0;
+for (let line of rpsList) {
+
+
+    if (line == "A X") {
+       ans += 3+0
+    }
+    else if (line == "A Y") {
+        ans += 1+3
+    }
+    else if (line == "A Z") {
+        ans += 2+6
+    }
+    if (line == "B X") {
+        ans += 1+0
+    }
+    else if (line == "B Y") {
+        ans += 2+3
+    }
+    else if (line == "B Z") {
+        ans += 3+6
+    }
+    if (line == "C X") {
+        ans += 2+0
+    }
+    else if (line == "C Y") {
+        ans += 3+3
+    }
+    else if (line == "C Z") {
+        ans += 1+6;
+    }
+
+}
+console.log(ans)
